e970cd4c952fc90bcca3805286af22e9d04b5a78
Parthib DS <parthib22.ds@gmail.com>
2024-02-28 12:11:56 +0530

    1

---
+++ DailyTemperature.py
@@ -0,0 +1,21 @@
+temp = [73, 74, 75, 71, 69, 72, 76, 73]
+answer = []
+i = 0
+while i < len(temp):
+    j = i + 1
+    while j < len(temp):
+        if temp[j] > temp[i]:
+            answer.append(j - i)
+            break
+        j += 1
+    if j == len(temp) - 1:
+        answer.append(0)
+    i += 1
+
+print(answer)
+
+# i = 0
+# while i < 10:
+#     i += 1
+#     print(i)
+# print(i)
